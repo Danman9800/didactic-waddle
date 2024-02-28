72b5abd8d6b32de6d9a3f9d602ee55ec8bb301f0
Danman9800 <adityaveram09@gmail.com>
2024-02-28 20:23:37 +0530

    Player can shoot now

--- Entities/Player.gd
+++ Entities/Player.gd
@@ -3,13 +3,24 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Crosshair.animation = "Idle"
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 
 func _process(_delta):
 	var mouse_pos := get_local_mouse_position()
 	$Crosshair.position = mouse_pos
 	$Rifle.position.x = lerp($Rifle.position.x, mouse_pos.x+5, 0.6)
+	
+	var targets_in = $Crosshair/HitZone.get_overlapping_areas()
+	
+	
 	if Input.is_action_just_pressed("Shoot"):
 		$Crosshair.animation = "Shoot"
+		for target in targets_in:
+			if target.is_in_group("targets"):
+				print("Got'em")
+				
 	elif Input.is_action_just_released("Shoot"):
 		$Crosshair.animation = "Idle"
+	
+	
